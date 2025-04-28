695a2805bf42da8c395143293b10db1704c41951
clioit <clioit01@gmail.com>
2025-04-27 20:49:58 -0700

    added bgm to POC scene

---
+++ White Whale/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // with code modified from https://www.youtube.com/watch?v=DU7cgVsU2rM
+    public static AudioManager instance;
+
+    [SerializeField] private AudioSource soundFXObject;
+    [SerializeField] private AudioSource musicSource;
+
+    public AudioClip bgm;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        musicSource.clip = bgm;
+        musicSource.Play();
+    }
+
+    public void PlaySoundClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        // spawn in gameObject
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+        // assign the audioClip
+        audioSource.clip = audioClip;
+
+        // assign volume
+        audioSource.volume = volume;
+
+        // play sound
+        audioSource.Play();
+
+        // // get length of sound effect clip
+        float clipLength = audioSource.clip.length;
+
+        // // destroy the clip after it is done playing
+        Destroy(audioSource.gameObject, clipLength);
+    }
+}
