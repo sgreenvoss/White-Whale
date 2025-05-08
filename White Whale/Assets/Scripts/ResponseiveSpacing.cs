1b05d267ec3af19c19fa0ed303cf3aab791c6c3c
dtiffany <bobdjt@gmail.com>
2025-05-08 15:54:10 -0700

    Created Responsive Spacing script for the UpgradeScene
    I def want to create a better version in the future but I think this fixes the root problem. Responsiveness is seen in game

---
+++ White Whale/Assets/Scripts/ResponseiveSpacing.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(VerticalLayoutGroup))]
+public class ResponsiveSpacing : MonoBehaviour
+{
+    private VerticalLayoutGroup layout;
+    private float referenceHeight = 1080f; // Use your design reference height
+
+    public float baseSpacing = 100f;
+
+    void Start()
+    {
+        layout = GetComponent<VerticalLayoutGroup>();
+        UpdateSpacing();
+    }
+
+    void Update()
+    {
+        UpdateSpacing(); // You could optimize by only calling on resolution change
+    }
+
+    void UpdateSpacing()
+    {
+        float currentHeight = Screen.height;
+        float scaleFactor = currentHeight / referenceHeight;
+        layout.spacing = baseSpacing * scaleFactor * 15;
+    }
+}
