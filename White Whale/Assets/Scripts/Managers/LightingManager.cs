7ba4eae5b7f1c2c1b500fa93db8f3ce4144be5dc
Julia Wheless <juliarwheless@gmail.com>
2025-05-25 13:41:01 -0700

    Working Goggles! (flashlight)

--- White Whale/Assets/Scripts/Managers/LightingManager.cs
+++ White Whale/Assets/Scripts/Managers/LightingManager.cs
@@ -1,17 +1,37 @@
-// using UnityEngine;
+using UnityEngine;
 
-// public class LightingManager : MonoBehaviour
-// {
-//     public Color noNightVision = 
-//     // Start is called once before the first execution of Update after the MonoBehaviour is created
-//     void Start()
-//     {
-        
-//     }
+public class LightingManager : MonoBehaviour
+{
+    public bool gogglesOn = false;
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-        
-//     }
-// }
+    private Color GoggleFogColor = new Color(0.1176f, 0.3255f, 0.4353f); 
+    private float GoggleFogDensity = 0.015f;
+
+    private Color defaultFogColor;
+    private float defaultFogDensity;
+
+    void Start()
+    {
+        defaultFogColor = RenderSettings.fogColor;
+        defaultFogDensity = RenderSettings.fogDensity;
+    }
+
+
+    void Update()
+    {
+        //this is for testing, will remove once implmented
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            PutGogglesOn();
+        }
+    }
+
+    void PutGogglesOn()
+    {
+        gogglesOn = true;
+        RenderSettings.fogColor = GoggleFogColor;
+        RenderSettings.fogDensity = GoggleFogDensity;
+        Debug.Log("Goggles On!");
+    }
+
+}
